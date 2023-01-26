8682eb01a89ceda05e18ef6963b0c7dc9fe4a9af
Tasreen Mowreen <tmowreen97@gmail.com>
2023-01-26 13:02:32 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :no_true_facts
+
+  def no_true_facts
+    if title=='True Facts'
+      errors.add(:title, "True Facts can not be the title!")
+    end
+  end
 end
